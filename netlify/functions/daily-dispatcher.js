e0095ce93d52378fe59cd903b19b60d8d80ed1aa
Eric <ehevelone@gmail.com>
2026-01-31 12:56:21 -0600

    Finalize daily dispatcher and rely on netlify.toml schedule

--- netlify/functions/daily-dispatcher.js
+++ netlify/functions/daily-dispatcher.js
@@ -3,10 +3,6 @@ import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 
-export const config = {
-  schedule: "*/2 * * * *" // every 2 minutes (testing)
-};
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
